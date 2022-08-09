9b3537aa145e2f90ba70ece85206c39eae7a25ca
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-08-09 10:37:22 +0200

    Update form-radio.php

--- views/admin/form-radio.php
+++ views/admin/form-radio.php
@@ -1,29 +1,33 @@
 <div class="<?php echo $this->class ?>">
 	<div id="<?php echo $this->element->getName() ?>" class="two columns alpha">
-	<?php echo $this->formLabel($this->element->getName(),
-				$this->element->getLabel(),
-				array("class"=>'optional')) ?>
+	<?php 
+		echo $this->formLabel($this->element->getName(),
+			$this->element->getLabel(),
+			array("class"=>'optional')
+		) 
+	?>
 	</div>
+
 	<div class="inputs five columns omega">
-	<p class="explanation"><?php echo $this->element->getDescription() ?></p>
+		<p class="explanation"><?php echo $this->element->getDescription() ?></p>
 
-<?php
-	$options = $this->element->getMultiOptions();
+		<?php
+			$options = $this->element->getMultiOptions();
 
-	foreach ($options as $option=>$value) {
-		echo '<field id="' . $this->element->getName() . '-' . $option . '-field">';
-		echo ('<input type="radio" name="' . $this->element->getName() . '" value="' . $option . '" id="' . $this->element->getName() . '-'.$option . '">');
-		echo ($value);
-		echo "</field>";
-	}
-?>
-	<?php echo $this->{$this->element->helper}(
-
-		$this->element->getName(),
-		$this->element->getValue(),
-		$this->element->getAttribs()
-	) ?>
-	<?php echo $this->formErrors($this->element->getMessages()) ?>
-
+			foreach ($options as $option=>$value) {
+				echo '<field id="' . $this->element->getName() . '-' . $option . '-field">';
+				echo ('<input type="radio" name="' . $this->element->getName() . '" value="' . $option . '" id="' . $this->element->getName() . '-'.$option . '">');
+				echo ($value);
+				echo "</field>";
+			}
+		?>
+		
+		<?php echo $this->{$this->element->helper}(
+			$this->element->getName(),
+			$this->element->getValue(),
+			$this->element->getAttribs()
+		) ?>
+		
+		<?php echo $this->formErrors($this->element->getMessages()) ?>
 	</div>
 </div>
